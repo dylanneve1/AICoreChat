b35588c91d8618f658460eb26a92c872454d6879
Dylan Neve <dylanneve1@gmail.com>
2025-09-15 21:11:48 +0100

    Bump version to 1.1 and adjust onboarding spacing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.dylanneve1.aicorechat"
         minSdk = 31
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0"
+        versionCode = 1
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
