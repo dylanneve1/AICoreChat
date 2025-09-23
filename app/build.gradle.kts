6443208c12f0b66ee97c4d564cf580e5d671e514
Dylan Neve <dylanneve1@gmail.com>
2025-09-23 19:27:05 +0100

    Bump app version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.dylanneve1.aicorechat"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.1"
+        versionCode = 11
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
